d79d56ad4518a6af88628dc0b70d2d0b2e8abfb6
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-03-16 21:13:57 +0700

    Penambahan pop up

--- components/alert.js
+++ components/alert.js
@@ -1,11 +1,11 @@
 function checkMobile() {
   if (window.innerWidth < 768) {
-    document.getElementById("warningPopup").style.display = "flex";
+    document.getElementById("warningOverlay").style.display = "flex";
   }
 }
 
 function closeWarning() {
-  document.getElementById("warningPopup").style.display = "none";
+  document.getElementById("warningOverlay").style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", checkMobile);
